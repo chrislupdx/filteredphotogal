86104f31d941b783b463fc42e53b50a3a262ab92
dreamer <wildetangeant@gmail.com>
2019-02-28 18:32:48 -0800

    filterd images moved to its own directory

---
+++ src/filtered-images.js
@@ -0,0 +1,6 @@
+export default function filteredImages(images, filter) {
+    return images.filter(image => {
+        const keyword = image.keyword === filter.keyword;
+        return keyword;
+    });
+};
--- test/file2.test.js
+++ test/file2.test.js
@@ -1,53 +1,49 @@
-const test = QUnit.test;
-QUnit.module('test filter parameters successfully passed');
-import images from '../data/images.js';
+const test = QUnit.test; 
+QUnit.module('DOMcontentTest');
+import filteredImages from '../src/filtered-images.js';
 
+const images = [
+    {
+        'title': 'UniWhal',
+        'keyword': 'narwhal',
+        'horns': 1
+    },
+    {
+        'title': 'Rhino Family',
+        'keyword': 'unicorn',
+        'horns': 2
+    },
+    {
+        'title': 'Unicorn Head',
+        'keyword': 'unicorn',
+        'horns': 1
+    }
+];
 
-function fetchFilteredPics(filterparams) {
-    keyWordInput = Document.getElementById('keywordInput');
-    console.log(keyWordInput);
-    const filteredImages = images.filter(function(image) {
-        return image.keyword === keyWordInput;
-    });    
-};
-
-    const dummyData = {
-        {
-            'url': 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
-            'title': 'UniWhal',
-            'description': 'A unicorn and a narwhal nuzzling their horns',
-            'keyword': 'narwhal',
-            'horns': 1
-        },
-        {
-            'url': 'https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80',
-            'title': 'Rhino Family',
-            'description': 'Mother (or father) rhino with two babies',
-            'keyword': 'rhino',
-            'horns': 2
-        }
+test('keyword filter test /whether i can filter objects out', function(assert){
+//arrange
+    const filter = {
+        keyword: 'unicorn'
     };
 
-    //use your dummy data in your test
-
-    const expected = [
+    const filtered = [
         {
-            'url': 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
-            'title': 'UniWhal',
-            'description': 'A unicorn and a narwhal nuzzling their horns',
-            'keyword': 'narwhal',
+            'title': 'Rhino Family',
+            'keyword': 'unicorn',
+            'horns': 2
+        },
+        {
+            'title': 'Unicorn Head',
+            'keyword': 'unicorn',
             'horns': 1
         }
-    ]
+    ];
 
-    test('can print the keyword requested pics', function (assert) {
-        //arrange
+    const result = filteredImages(images, filter);
+   
+//act
 
-       
 
-        //act
-        const result = fetchFilteredPics(dummyData);
-
-        //assert
-        assert.deepEqual(result, expected);
-    });
+//assert
+    assert.deepEqual(filtered, result);
+});
